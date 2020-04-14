5b4a8123d8960ac1a118c78377d88ac7ad43588b
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-04-14 20:49:54 +0100

    adding axios and fetching the coronavirus data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import L from "leaflet";
+import axios from "axios";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
@@ -20,8 +21,20 @@ const IndexPage = () => {
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement } = {}) {}
+  async function mapEffect({ leafletElement: map } = {}) {
+    if (!map) return;
 
+    let response;
+
+    try {
+      response = await axios.get("https://corona.lmao.ninja/countries");
+    } catch (e) {
+      console.log(`Failed to fetch countries: ${e.message}`, e);
+      return;
+    }
+
+    const { data = [] } = response;
+  }
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",

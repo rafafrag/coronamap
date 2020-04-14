5f116fdf90c11b8b7fed3c8a9e50594f14319496
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-04-14 22:27:03 +0100

    creating geographic data coordenates per country

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,31 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    // const geoJsonLayers = new L.GeoJSON(geoJson);
+
+    // geoJsonLayers.addTo(map);
   }
   const mapSettings = {
     center: CENTER,

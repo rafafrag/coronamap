a9e9f2069246e38b192a590aecc9f82f8724c919
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-04-14 23:05:35 +0100

    adding the coronavirus data to the map and styling the markers

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Container from "components/Container";
 import Map from "components/Map";
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -32,7 +32,6 @@ const IndexPage = () => {
       console.log(`Failed to fetch countries: ${e.message}`, e);
       return;
     }
-
     const { data = [] } = response;
     const hasData = Array.isArray(data) && data.length > 0;
 
@@ -56,9 +55,50 @@ const IndexPage = () => {
       }),
     };
 
-    // const geoJsonLayers = new L.GeoJSON(geoJson);
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
 
-    // geoJsonLayers.addTo(map);
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
   }
   const mapSettings = {
     center: CENTER,
